383d4497482b3c3da69156ed09fafced7c30e200
mwmac88 <mwmaclean88@googlemail.com>
2021-06-24 21:34:47 +0100

    random number from hello
    lodash installed

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { random } from 'lodash';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  random: number
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.query)
+  const randomNum = random(0.5, 10);
+  res.status(200).json({ random: randomNum })
 }
